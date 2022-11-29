212c82e7529cf8c2f9111bde23bd5a05459b9382
xicobski <115943937+xicobski@users.noreply.github.com>
2022-11-29 18:15:28 +0000

    Add files via upload

--- neeicum_user-master/lib/HomePage/qr.dart
+++ neeicum_user-master/lib/HomePage/qr.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class QrPage extends StatefulWidget {
   const QrPage({super.key});
@@ -12,12 +14,53 @@ class QrPage extends StatefulWidget {
 
 class _QrPageState extends State<QrPage> {
   String logoCurso = "assets/images/logo_w.png";
+
+  final qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  Barcode? barcode;
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
         actions: [
+          IconButton(
+              onPressed: () async {
+                await controller?.toggleFlash();
+              },
+              icon: FutureBuilder<bool?>(
+                  future: controller?.getFlashStatus(),
+                  builder: (context, snapshot) {
+                    if (snapshot.data != null) {
+                      return Icon(snapshot.data!
+                          ? Icons.flash_on_rounded
+                          : Icons.flash_off_rounded);
+                    } else {
+                      return Container();
+                    }
+                  })),
+          IconButton(
+              onPressed: () async {
+                await controller?.resumeCamera();
+              },
+              icon: Icon(Icons.flip_camera_ios_rounded)),
           IconButton(onPressed: null, icon: const Icon(Icons.qr_code_scanner)),
         ],
         title: Padding(
@@ -41,8 +84,44 @@ class _QrPageState extends State<QrPage> {
             ],
           ),
         ),
-        //backgroundColor: Colors.indigo,
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          buildQrView(context),
+          Positioned(bottom: 10, child: buildResult())
+        ],
       ),
     );
+  }
+
+  Widget buildResult() => Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.grey[400],
+        ),
+        child: Text(
+          barcode != null ? 'Result:  ${barcode!.code}' : 'Scan a code!',
+          maxLines: 3,
+        ),
+      );
+
+  Widget buildQrView(BuildContext context) => QRView(
+        key: qrKey,
+        onQRViewCreated: onQRViewCreated,
+        overlay: QrScannerOverlayShape(
+          cutOutSize: MediaQuery.of(context).size.width * 0.8,
+          borderWidth: 10,
+          borderLength: 20,
+          borderRadius: 10,
+          borderColor: Colors.indigo,
+        ),
+      );
+
+  void onQRViewCreated(QRViewController controller) {
+    setState(() => this.controller = controller);
+    controller.scannedDataStream
+        .listen((barcode) => setState(() => this.barcode = barcode));
   }
 }
