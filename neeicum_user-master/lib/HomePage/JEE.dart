69b0919689bb6447853e3b2637203c3caf592d18
RuiRocha23 <96590570+RuiRocha23@users.noreply.github.com>
2022-12-20 17:44:34 +0000

    Update JEE.dart

--- neeicum_user-master/lib/HomePage/JEE.dart
+++ neeicum_user-master/lib/HomePage/JEE.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+mport 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:qr_flutter/qr_flutter.dart';
